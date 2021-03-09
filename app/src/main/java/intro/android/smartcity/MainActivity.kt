778ff0ece0971a8a0bff767a6db5d040785c3b17
Tiago Marinho <tiago.marinho31@hotmail.com>
2021-03-09 17:09:55 +0000

    Initial commit

---
+++ app/src/main/java/intro/android/smartcity/MainActivity.kt
@@ -0,0 +1,11 @@
+package intro.android.smartcity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
